772a6189df5d5a51a0608b54269cd665dcc2de31
adarleika <adarleika@gmail.com>
2025-10-03 04:39:41 -0300

    creacion de ejercicio de prueba, dependencias y archivos img priorizando mobile first en una primera resolucion de mobile

---
+++ src/components/Slider.vue
@@ -0,0 +1,15 @@
+
+
+<template>
+        <div class="flex flex-row justify-around items-center">
+            
+            <button @click="prevSlide" class="top-1/2 left-2 ...">Prev</button>
+            <button @click="nextSlide" class="top-1/2 right-2 ...">Next</button>
+            <button @click="pauseSlide" class="top-1/2 right-2 ...">Pause</button>
+
+        </div>
+
+
+</template>
+
+
