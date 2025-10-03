12c7a75e044819019c2c727604fd4c746da21d48
adarleika <adarleika@gmail.com>
2025-10-03 12:05:59 -0300

    se agregan nuevos estilos para mobile, visibilidad de textos e imagenes por resolucion segun aplique

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,18 @@ import Slider from './components/Slider.vue';
 
     <div class="menu flex flex-row justify-around items-center">
 
-      <div class="">
-        <img src="./assets/img/png/logo_mob.png" alt="" srcset="./assets/img/svg/logo_mob.svg">
+      <div class="p-5">
+        <img src="./assets/img/png/logo_mob.png" alt="imagen logo" srcset="./assets/img/svg/logo_mob.svg">
       </div>
 
+      <div class="md:contents md:flex-row justify-around invisible md:visible">
+        <p class="text-produc-tarifas">PRODUCTOS Y TARIFAS </p>
+
+        <p class="text-contratar">COMO CONTRATAR </p>
+
+        <p class="text-beneficios">BENEFICIOS</p>
+
+      </div>
       
       <div class="">
         <button class="btn-contratar">
@@ -18,7 +26,7 @@ import Slider from './components/Slider.vue';
       </div>
 
       <div class="">
-        <img src="./assets/img/png/menu_mob.png" alt="" srcset="./assets/img/svg/menu_mob.svg">
+        <img src="./assets/img/png/menu_mob.png" alt="imagen menu" srcset="./assets/img/svg/menu_mob.svg">
       </div>
 
 
@@ -27,14 +35,15 @@ import Slider from './components/Slider.vue';
 
     <div class="fondo">
 
-      <div class="huincha flex flex-row justify-around items-center">
-        <img src="./assets/img/png/truck_fast.png" alt="" srcset="./assets/img/svg/truck_fast.svg">
+      <div class="huincha flex justify-center items-center">
+        <img src="./assets/img/png/truck_fast.png" alt="imagen truck fast" srcset="./assets/img/svg/truck_fast.svg">
 
         <p class="text-huincha">¡Contrata hoy y obtén los mejores beneficios</p>
       </div>
 
       <div class="img-chico flex justify-around items-center p-5">
-        <img src="./assets/img/png/chico_mob.png" alt="">
+        <img class="visible md:invisible" src="./assets/img/png/chico_mob.png" alt="imagen chico">
+        <img class="invisible md:visible" src="./assets/img/png/chico_tablet.png" alt="imagen chico">
       </div>
       
       <div class="text-center m-2">
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,15 +1,80 @@
+<script setup>
+import { ref } from 'vue';
 
+// 1. Definir la lógica del Slider
+const currentSlide = ref(0); // Para saber qué slide se está mostrando
+const isPaused = ref(false); // Para saber si está pausado
+
+// 2. Definir las funciones que se usan en el <template>
+
+function prevSlide() {
+  console.log("Diapositiva anterior");
+  // Aquí va la lógica para ir a la diapositiva anterior
+  // Ejemplo: currentSlide.value = ...
+}
+
+function nextSlide() {
+  console.log("Siguiente diapositiva");
+  // Aquí va la lógica para ir a la siguiente diapositiva
+  // Ejemplo: currentSlide.value = ...
+}
+
+function pauseSlide() {
+  console.log("Pausar/Reanudar");
+  // Aquí va la lógica para pausar el carrusel
+  isPaused.value = !isPaused.value; // Alternar el estado
+}
+
+// Opcional: Si necesitas pasar estas funciones a otro componente (no lo necesitas en el botón), 
+// no es necesario hacer 'return' en <script setup>, se exponen automáticamente.
+</script>
 
 <template>
-        <div class="flex flex-row justify-around items-center">
-            
-            <button @click="prevSlide" class="top-1/2 left-2 ...">Prev</button>
-            <button @click="nextSlide" class="top-1/2 right-2 ...">Next</button>
-            <button @click="pauseSlide" class="top-1/2 right-2 ...">Pause</button>
+  <div class="fixed bottom-6 left-0 right-0 flex justify-center items-center space-x-6">
+    
+    <button @click="prevSlide" class="prevSlide m-2">
+        &lt;
+    </button>
+    
+    <button @click="nextSlide" class="nextSlide m-2">
+        &gt;
+    </button>
+    
+    <button class="pauseSlide">
+        <span :class="{'text-white': !isPaused, 'text-teal-400': isPaused}" class=" text-3xl">
+            | |
+        </span>
+    </button>
 
-        </div>
-
-
+    
+  </div>
 </template>
+
+<style scoped>
+
+    .prevSlide, .nextSlide {
+        background-color: #0A1861;
+        color: #ffffff;
+        border-radius: 43px;
+        width: 56px;
+        height: 56px;
+        font-size: 30px;
+
+    }
+
+    .pauseSlide {
+
+        width: 56px;
+        height: 56px;
+
+        span {
+            font-size: 15px;
+        }
+    }
+
+
+</style>
+
+
 
 
